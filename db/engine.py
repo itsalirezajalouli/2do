3c3162d7d9752a5642e606bad0d142c7f4be58be
Billy Pu$her <billypushr@gmail.com>
2026-06-19 21:35:27 +0330

    Fix install.sh

--- db/engine.py
+++ db/engine.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 from sqlmodel import SQLModel, create_engine
 
 load_dotenv()
-engine = create_engine(f'sqlite:///{os.getenv("SQLITE_FILE_ADDR")}', echo=False)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'database.db')
+engine = create_engine(f'sqlite:///{DB_PATH}', echo=False)
 
 
 def get_engine():
